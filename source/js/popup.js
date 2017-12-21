1bc5eb48973cee648ecaca5df9fde999dc759885
DenisSeroshtan <haron_777@mail.ru>
2017-12-22 03:52:49 +0500

    add preloader

--- source/js/popup.js
+++ source/js/popup.js
@@ -1,5 +1,5 @@
 
-function Popup(obj) { // make prototype
+function Popup(obj) { // prototype
   var popup = this;
 
   var overlay = document.createElement('div');
@@ -7,6 +7,8 @@ function Popup(obj) { // make prototype
   var btnClose = document.createElement('div');
   var modalContent = document.createElement('div');
 
+  var body = document.querySelector("body");
+  var duration = obj.duration || 4000;
   // создаем элементы DOM при загрузке документа
   document.body.insertBefore(overlay, document.body.children[0]);
   overlay.className = "overlay";
@@ -24,24 +26,27 @@ function Popup(obj) { // make prototype
   this.modal = document.querySelector(".modal");
   this.modalContent = document.querySelector(".modal .content");
   this.btnClose = document.querySelector(".modal .close");
-
+  this.trigger = document.querySelectorAll(obj.trigger);
 
   this.resize = function () {
     // расчет высоты overlay по высоте body
-    var b = document.querySelector('body');
-    var bH = b.offsetHeight;
+    var bH = body.offsetHeight;
     popup.overlay.style.height = bH + "px";
   };
   popup.resize();
+
   //изменение высоты overlay при изменение размера окна
   window.onresize = popup.resize;
 
-  this.trigger = document.querySelectorAll(obj.trigger);
-
   this.open = function (cont) {
-    if (obj.content !== undefined) {
+    if (obj.content) {
       cont = document.querySelector(obj.content).innerHTML
+    } else if (cont) {
+      cont =  document.querySelector(cont).innerHTML
+    } else {
+      cont = "здесь должен быть контент"
     }
+
 
     // выравнивание по вертикали
     var wH= document.documentElement.clientHeight;
@@ -52,26 +57,34 @@ function Popup(obj) { // make prototype
     popup.overlay.classList.add('showed');
     popup.modal.classList.add('showed');
 
+    body.style.overflow = 'hidden';
+
   };
-//		popup.btn.addEventListener('click', openPopup.bind(this, cont), false);
   this.close = function () {
     popup.overlay.classList.remove('showed');
     popup.modal.classList.remove('showed');
     popup.modal.style.top = 0;
+    body.style.overflow = 'visible';
 
   };
+  if (obj.auto) {
+    setTimeout(function () {
+      popup.open()
+    }, duration)
+  }
   // применяем настройки по ключам объекта
-  if (obj.trigger !== undefined) {
+  if (this.trigger) {
     for(var i = 0; i<this.trigger.length; i++) {
       this.trigger[i].addEventListener('click', function(e){
         e.preventDefault();
+        // проверям наличие ссылки на popup
         if (this.hasAttribute('data-popup')) {
           var link = this.getAttribute('data-popup');
           var form = document.querySelector(link);
           popup.open(form.innerHTML);
+        }else{
+          popup.open();
         }
-        popup.open();
-
       })
     }
   }
@@ -83,17 +96,20 @@ function Popup(obj) { // make prototype
 var popup = new Popup({
   content: ".header-mid__right",
   trigger: ".section__bottom .btn"
+  // auto: true
+  // duration: 12000
 });
-(function () {
 
-  var buttons = document.querySelectorAll(".section__bottom .btn");
+(function () {
+  // меняем заголовки в popup
+  var buttons = popup.trigger;
   for (var k = 0; k<buttons.length; k++) {
-    buttons[k].onclick = function (e) {
+    buttons[k].addEventListener('click', function (e) {
       e.preventDefault();
       var text = this.innerHTML;
       var title = document.querySelector('.form__title');
       title.innerHTML = text;
-    }
+    })
   }
 }());
 
--- source/js/scroll.js
+++ source/js/scroll.js
@@ -1,16 +1,8 @@
+window.onload = function() {
+  document.querySelector('.wrapper').classList.add('loaded');
+};
+
 (function(){
- //show btn scroll to top
-  $(document).scroll(function () {
-    var currentHeight = $(this).scrollTop(),
-        hHeader = $('#js-header').height(),
-        top = $('.top');
-    if (currentHeight > hHeader) {
-      top.addClass('active')
-    }
-    else {
-      top.removeClass('active')
-    }
-  });
  //smooth scroll
   $('a[href^="#js"]').on('click', function(e) {
     e.preventDefault();
